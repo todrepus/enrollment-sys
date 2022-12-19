fc985689d428a6673025859751721e2bc7207d8a
KANGHYUNWOO <superdot@naver.com>
2022-12-19 20:29:29 +0900

    fixed untracked files

---
+++ backend/enrollment-sys/src/main/java/com/todrepus/enrollmentsys/web/HomeController.java
@@ -0,0 +1,11 @@
+package com.todrepus.enrollmentsys.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class HomeController {
+
+}
