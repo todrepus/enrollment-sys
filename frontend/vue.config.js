e1bdb5039c2d3b32fcc549ebb068cf7109902f04
KANGHYUNWOO <superdot@naver.com>
2022-12-19 20:28:56 +0900

    fixed untracked files

--- frontend/vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: "../backend/enrollment-sys/src/main/resources/static",
-  devServer: {
-    port: 4545,
-    proxy: "http://localhost:8080"
-  }
-})
